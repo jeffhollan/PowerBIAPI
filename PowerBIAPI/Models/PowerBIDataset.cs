a83aa63c2985c97a16121befe635094cfed14d63
Jeff Hollan <jeffhollan@outlook.com>
2015-10-02 08:51:05 -0700

    adding dataset

---
+++ PowerBIAPI/Models/PowerBIDataset.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PowerBIAPI.Models
+{
+    public class PowerBIDataset
+    {
+        public string name { get; set; }
+        public List<Tables> tables { get; set; }
+
+        public class Tables
+        {
+            public string name { get; set; }
+            public List<Columns> columns { get; set; }
+
+            public class Columns
+            {
+                public string name { get; set; }
+                public string dataType { get; set; }
+            }
+        }
+    }
+}
